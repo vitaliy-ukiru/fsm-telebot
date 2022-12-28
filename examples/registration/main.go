d276bd1b1da2a904220b591c74c1f30c40cefa3b
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2022-12-28 15:22:09 +0500

    add field bot Manager, add support groups for manager

--- examples/registration/main.go
+++ examples/registration/main.go
@@ -47,7 +47,7 @@ func main() {
 	storage := memory.NewStorage()
 	defer storage.Close()
 
-	manager := fsm.NewManager(bot.Group(), storage)
+	manager := fsm.NewManager(bot, nil, storage)
 
 	var (
 		regBtn    = tele.Btn{Text: "📝 Start input form"}
