7397d43047bd06b21183f2825cd7a83367db418e
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-06-03 23:54:14 +0500

    test: using test cases

--- storages/redis/redis_test.go
+++ storages/redis/redis_test.go
@@ -7,22 +7,55 @@ import (
 )
 
 func TestStorage_generateKey(t *testing.T) {
-	s := Storage{prefix: "test"}
-
-	assert.Equal(t,
-		"test:1:1:data:myKey",
-		s.generateKey(1, 1, stateDataKey, "myKey"),
-	)
-	assert.Equal(t,
-		"test:1:1:data:multiple:keys",
-		s.generateKey(1, 1, stateDataKey, "multiple", "keys"),
-	)
-	assert.Equal(t,
-		"test:1:1:data:*",
-		s.generateKey(1, 1, stateDataKey, "*"),
-	)
-	assert.Equal(t,
-		"test:1:1:state",
-		s.generateKey(1, 1, stateKey),
-	)
+	s := &Storage{pref: StorageSettings{Prefix: "test"}}
+	type args struct {
+		chat    int64
+		user    int64
+		keyType keyType
+		keys    []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "simple key",
+			args: args{
+				keyType: dataKey,
+				keys:    []string{"myKey"},
+			},
+			want: "test:0:0:data:myKey",
+		},
+		{
+			name: "multiple key",
+			args: args{
+				keyType: dataKey,
+				keys:    []string{"multiple", "keys"},
+			},
+			want: "test:0:0:data:multiple:keys",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(
+				t,
+				tt.want,
+				s.generateKey(tt.args.chat, tt.args.user, tt.args.keyType, tt.args.keys...),
+				"generateKey(%v, %v, %v, %v)",
+				tt.args.chat,
+				tt.args.user,
+				tt.args.keyType,
+				tt.args.keys,
+			)
+		})
+	}
+	t.Run("key type", func(t *testing.T) {
+		assert.NotEqual(
+			t,
+			s.generateKey(0, 0, stateKey),
+			s.generateKey(0, 0, dataKey, "state"),
+			"state key and data[state] equals",
+		)
+	})
 }
