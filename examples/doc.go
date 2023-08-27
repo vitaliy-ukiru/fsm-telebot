3b95bc49edbd89b4735b16bb0cde5c2d9c034a66
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-08-27 13:41:33 +0500

    docs: add file_storage example description

--- examples/doc.go
+++ examples/doc.go
@@ -5,4 +5,9 @@
 //
 // The "registration" package contains example of input form.
 // In this example uses storage, state groups, managing state flow.
+//
+// The "file_storage" package contains example of use file.Storage with provider.PrettyJson
+// The example shows how to initialize the file storage, shows how to work with
+// user data, use helper function to get WriterFunc,
+// there is also a simple graceful shutdown.
 package examples
