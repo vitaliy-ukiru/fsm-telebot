f6ded83153ee56933b6d8414fe837046887a5c76
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-06-04 22:33:50 +0500

    docs(storages): add info about redis storage

--- pkg/storage/doc.go
+++ pkg/storage/doc.go
@@ -4,4 +4,17 @@
 //
 // This is concurrency safe in-memory storage.
 // It'll be reset on shutdown.
+//
+// # Redis storage
+//
+// This storage based in redis.
+//
+// Distribute on separate module as
+// [github.com/nacknime-official/fsm-telebot-redis-storage/v2]
+//
+// All issues/pull request send to this [repository].
+// Please note that this repository separates major
+// versions by directory.
+//
+// [repository]: https://github.com/nacknime-official/fsm-telebot-redis-storage
 package storage
